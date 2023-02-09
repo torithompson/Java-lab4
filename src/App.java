bff2a1071bb163c3ca161a66c36ac9e9f6ba77f9
Tori <tori.thompson@student.sl.on.ca>
2023-02-09 18:10:13 -0500

    added hashmap

--- src/App.java
+++ src/App.java
@@ -4,28 +4,48 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class App {
 
     static ArrayList<Player> playerList = new ArrayList<>();
+    static HashMap<String, Sport> sportMap = new HashMap<>();
 
     /**
      * Players and sports application
      * <br>
-     * This application reads information consisting of player names and the sports they play 
-     * from an input file.   
-     * Each line of the input file must consist of a player name followed by a comma-separated 
-     * list of sports.  A new player object is created from the data contained in each line of the file.
+     * This application reads information consisting of player names and the sports
+     * they play
+     * from an input file.
+     * Each line of the input file must consist of a player name followed by a
+     * comma-separated
+     * list of sports. A new player object is created from the data contained in
+     * each line of the file.
      * Each new player object is added to an ArrayList of Player objects
      * <br>
      * 
-     * @param args - args[0] will hold name of input file (to be done in lab)
+     * @param args
      */
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         loadPlayerFile();
+        initSportMap();
         System.out.println(playerList);
+        System.out.println(sportMap);
     }
-    
+
+    private static void initSportMap() {
+        for (Player player : playerList) {
+            ArrayList<String> sportsList = player.getSports();
+            for (String sportName : sportsList) {
+                Sport sport = sportMap.get(sportName);
+                if (sport == null) {
+                    sport = new Sport(sportName);
+                    sportMap.put(sportName, sport);
+                }
+                sport.addPlayer(player.getName());
+            }
+        }
+    }
 
     /**
      * 
@@ -34,48 +54,41 @@ public class App {
      * 
      * 1 - Open input and output files
      * 2 - For each line of input
-     * 3 -- split line into tokans
+     * 3 -- split line into tokens
      * 4 -- create new player whose name is given by token[0]
-     * 5 -- add remaining tokens to player's list of sports 
+     * 5 -- add remaining tokens to player's list of sports
      * 6 - add player to player list
      * 
      */
 
     private static void loadPlayerFile() {
-        File file = new File("res/players.txt");
-        try{
-            // 1            
+        File file = new File("Lab4 Starter/res/players.txt");
+        try {
+            // 1
             FileReader fileReader = new FileReader(file);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
 
             // 2
             String line = bufferedReader.readLine();
-            while (line != null)
-            {
-                String[] tokens = line.split(",");              // 3
-                Player player = new Player(tokens[0].trim());   // 4
-                for (int i = 1; i < tokens.length; i++) {       // 5
+            while (line != null) {
+                String[] tokens = line.split(","); // 3
+                Player player = new Player(tokens[0].trim()); // 4
+                for (int i = 1; i < tokens.length; i++) { // 5
                     String sport = tokens[i].trim();
                     player.addSport(sport);
-                }                
-                playerList.add(player);                         // 6
+                }
+                playerList.add(player); // 6
 
-                line = bufferedReader.readLine();               
+                line = bufferedReader.readLine();
             }
             bufferedReader.close();
-        }
-        catch(NullPointerException e)
-        {
+        } catch (NullPointerException e) {
             System.err.println("loadPlayerFile");
             System.err.println(e.getMessage());
-        }
-        catch (FileNotFoundException e)
-        {
+        } catch (FileNotFoundException e) {
             System.err.println("Check the input file name");
             e.printStackTrace();
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             System.out.println("Problem reading data from input file");
             e.printStackTrace();
         }
--- src/Sport.java
+++ src/Sport.java
@@ -1,28 +1,35 @@
 import java.util.ArrayList;
 
+/*Sport class - holds sport name and an array of list of players */
 public class Sport {
 
     String sportName;
 
     ArrayList<String> playerList;
 
+    /**
+     *
+     * @param sportName - string representing name of the sport
+     */
     public Sport(String sportName) {
-        super();
-        playerList = new ArrayList<>();
         this.sportName = sportName;
+        playerList = new ArrayList<>();
     }
 
-    public void addPlayerName(String name)
-    {
-        playerList.add(name);
+    /**
+     * 
+     * @param name - name of the player to be added to the player list
+     */
+    public void addPlayer(String playerName) {
+        playerList.add(playerName);
     }
 
+    /**
+     * This is an override for toString method
+     */
     @Override
     public String toString() {
-        return  playerList.toString() ;
+        return playerList.toString();
     }
-        
-    
-    
 
 }
