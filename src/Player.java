70e36eacb07e4adb932ab3cfa245228f06afa2d3
Tori <tori.thompson@student.sl.on.ca>
2023-02-09 18:13:52 -0500

    Added comments

--- src/Player.java
+++ src/Player.java
@@ -1,33 +1,54 @@
 import java.util.ArrayList;
 
 public class Player {
-    
+
     String name;
     ArrayList<String> sports;
 
+    /**
+     * Constructor for objects of class Player
+     * 
+     * @param name
+     */
     public Player(String name) {
         this.name = name;
-        sports =  new ArrayList<>();
-    } 
+        sports = new ArrayList<>();
+    }
 
-    public void addSport(String sport)
-    {
+    /**
+     * Add a sport to the player's list of sports
+     * 
+     * @param sport
+     */
+    public void addSport(String sport) {
         sports.add(sport);
     }
 
+    /**
+     * Get the name of the player
+     * 
+     * @return name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Get the list of sports the player plays
+     * 
+     * @return sports
+     */
     public ArrayList<String> getSports() {
         return sports;
     }
 
+    /**
+     * Get the number of sports the player plays
+     * 
+     * @return number of sports
+     */
     @Override
     public String toString() {
         return "\n" + name + ": " + sports.toString();
-    }    
-
-
-    
+    }
 }
